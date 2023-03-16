43fcbfb37a5fdabacf724d73ed4a0b183be265c4
James Trigg <james@jamestrigg.co.uk>
2023-03-16 18:02:56 +0000

    Initial Commit

---
+++ AICodeDocumention/AICodeDocumentionApp.swift
@@ -0,0 +1,20 @@
+//
+//  AICodeDocumentionApp.swift
+//  AICodeDocumention
+//
+//  Created by James Trigg on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AICodeDocumentionApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
